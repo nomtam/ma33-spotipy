abdf3599cefed65fd2e30f1c9bd21e4247527b4a
Tomer <kadison.tomer@gmail.commy>
2022-02-09 10:27:04 +0200

    finishng the loading for now

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
+import configuration
 from Data import Data
 from SongLoader import SongLoader
 data = Data()
 loader = SongLoader(data)
-loader.load_songs_in_directory('Songs')
-print(data)
+loader.load_songs_in_directory(configuration.songs_path)
+for artist in data.artists.values():
+    print(artist)
